a3e31411fef246a624410a58f5b3f9845a120546
Derek Wan <d.wan@icloud.com>
2023-05-26 19:13:15 +0800

    Finish lesson hooks 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,13 @@
-import { useState } from "react";
+import { useEffect } from "react";
 
-interface TagState {
-  tagSelected: number | null;
-  tags: { id: number; value: string }[];
-}
+export const useTimeout = (timerMs: number) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("Done!");
+    }, timerMs);
 
-export const Tags = () => {
-  const [state, setState] = useState<TagState>({
-    tags: [],
-    tagSelected: null,
-  });
-  return (
-    <div>
-      {state.tags.map((tag) => {
-        return (
-          <button
-            key={tag.id}
-            onClick={() => {
-              setState(
-                (currentState): TagState => ({
-                  ...currentState,
-                  // @ts-expect-error
-                  tagselected: tag.id,
-                })
-              );
-            }}
-          >
-            {tag.value}
-          </button>
-        );
-      })}
-      <button
-        onClick={() => {
-          setState(
-            (currentState): TagState => ({
-              ...currentState,
-              tags: [
-                ...currentState.tags,
-                {
-                  id: new Date().getTime(),
-                  value: "New",
-                  // @ts-expect-error
-                  otherValue: "something",
-                },
-              ],
-            })
-          );
-        }}
-      >
-        Add Tag
-      </button>
-    </div>
-  );
+    return () => clearTimeout(timer);
+  }, [timerMs]);
 };
 
 function App() {
