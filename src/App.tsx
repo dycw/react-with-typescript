83f8b904d44fc72504ea419cc934e85c77fe94f6
Derek Wan <d.wan@icloud.com>
2023-05-26 20:31:03 +0800

    Finish lesson hooks 10 (#16)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { useRef } from "react";
+import { useReducer } from "react";
 
-export const Component = () => {
-  const ref = useRef<string>(null);
+type Add = { type: "add"; add: number };
+type Subtract = { type: "subtract"; subtract: number };
 
-  // Why is this not allowed?
-  ref.current = "Hello";
-
-  return null;
+const reducer = (state: { count: number }, action: Add | Subtract) => {
+  switch (action.type) {
+    case "add":
+      return { count: state.count + action.add };
+    case "subtract":
+      return { count: state.count - action.subtract };
+    default:
+      throw new Error();
+  }
 };
+
+const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+dispatch({ type: "add", add: 1 });
+
+// @ts-expect-error
+dispatch({ type: "SUBTRACT", subtract: 1 });
+
+// @ts-expect-error
+dispatch({ type: "add" });
+
+// @ts-expect-error
+dispatch({ type: "subtract", subtract: "123" });
 
 function App() {
   return <div></div>;
