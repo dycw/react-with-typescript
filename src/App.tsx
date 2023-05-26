f04d470ee26a11c14f53622b310fca16437f967e
Derek Wan <d.wan@icloud.com>
2023-05-26 17:48:24 +0800

    Finish lesson components 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
 
-export const Button = (props: { children: React.ReactNode }) => {
-  return <button>{props.children}</button>;
-};
+interface ButtonProps {
+  className: string;
+  children: React.ReactNode;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
 
-const Parent = () => {
+export const Button = ({ children, className, onClick }: ButtonProps) => {
   return (
-    <>
-      {/* @ts-expect-error */}
-      <Button></Button>
-      <Button>Hello world!</Button>
-    </>
+    <button onClick={onClick} className={className}>
+      {children}
+    </button>
   );
 };
 
